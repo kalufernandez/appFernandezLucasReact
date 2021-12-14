64d5962fc2b0c053b15647f3d55c4c4aeaeb59e1
Lucas Fernandez <lucas.emiliano.fernandez@gmail.com>
2021-12-14 20:32:20 -0300

    Entrega Clase 5 - Componentes II

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+
 import NavBar from "./Componentes/NavBar"
 import ItemListContainer from "./Componentes/ItemListContainer";
+import ItemCount from "./Componentes/ItemCount";
 import "./scss/app.scss"
 
 
 function App () {
+    
+
+
     return (
         <>
             <NavBar />
             <ItemListContainer greetings="Bienvenidos"/>
             <main>Mi main</main>
+            <ItemCount stock={5} initial={1} onAdd/>
         </>
     )
 }
---
+++ src/Componentes/ItemCount.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+
+
+
+
+function ItemCount ({initial, stock, onAdd}) {
+    let [contador, setcontador] = useState(0)
+
+
+
+    const sumar = () => {
+        if {contador} >= 0 && {contador} <= {stock} { 
+            setcontador(contador+1) //Le paso como parametro contador + lo que quiero que aumente. Siempre se tiene que ejecutar primero la funcion.
+            return contador
+        }
+    } 
+
+    const restar = () => {
+        if {contador} == 1 {
+            setcontador(contador+1)
+            return contador
+        
+        }
+    } 
+
+    const onAdd = () => {
+        console.log("Enviado al Carrito")
+    }
+
+
+    return (
+        <>
+            <p>contador: {contador}</p>
+            <button onClick={sumar}>Sumar</button>
+            <button onClick={restar}>Restar</button>
+            <button onClick={onAdd}>Carrito</button>
+        </>
+    )
+}
+
+export default ItemCount
